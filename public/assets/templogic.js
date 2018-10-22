da0b592e13b3c85f228f6167aa7c8aacf2fc9e37
eric taft <erict54757@gmail.com>
2018-10-22 01:24:02 -0400

    m

--- public/assets/templogic.js
+++ public/assets/templogic.js
@@ -6,7 +6,7 @@ let userNameInput = "";
 let genderValInput = "";
 let ageGroupInput = "";
 var scoreArr = [];
-var scoreArr2=[];
+var scoreArr2 = [];
 
 
 let validateAndStore = {
@@ -23,9 +23,9 @@ let validateAndStore = {
         } else if (userName.val().length > 20) {
             alert("Username has too many characters");
             return $("#userName").focus();
-    }
-       userNameInput = userName.val().trim().replace(/\s+/g, '');
-        
+        }
+        userNameInput = userName.val().trim().replace(/\s+/g, '');
+
         validateAndStore.validateGender();
 
     },
@@ -65,22 +65,22 @@ let validateAndStore = {
     },
     nextpage: function next() {
         $("#next").data("value", "secondpage");
-       
-        $.ajax ({
+
+        $.ajax({
             type: "POST",
             url: "/survey000",
-            data: {userNameInput, genderValInput,ageGroupInput},
+            data: { userNameInput, genderValInput, ageGroupInput },
             dataType: "json"
-    
+
         });
-            window.location.href = "/survey1";
-        
-        
-       
+        window.location.href = "/survey1";
+
+
+
     },
     //score function checks all my first four questions and pushes the result in order to scoreArr (loop d loop)
     question1234: function score() {
-        scoreArr=[];
+        scoreArr = [];
         for (i = 1; i < 5; i++) {
 
             for (let j = 1; j < 6; j++) {
@@ -93,33 +93,33 @@ let validateAndStore = {
                     question[i] = $("#question" + [i] + [j]).val();
                     scoreArr.push(question[i]);
                     number = i + 1;
-                 }
+                }
             }
             // validation of all questions answered, highlight next question to be answered
         } if (scoreArr.length < 4) {
             alert("You must answer all questions to proceed"); scoreArr = []; $(".word" + [number]).focus();
-        }if (scoreArr.length===4){ 
-        console.log(scoreArr)
+        } if (scoreArr.length === 4) {
+            console.log(scoreArr)
 
-    scoreArr.forEach(element => {
-        
-  
+            scoreArr.forEach(element => {
 
-        $.ajax ({
-            type: "POST",
-            url: "/survey111",
-            data:{ score: element},
-            dataType: "json"
-    
-        });
-    });
-    
+
+
+                $.ajax({
+                    type: "POST",
+                    url: "/survey111",
+                    data: { score: element },
+                    dataType: "json"
+
+                });
+            });
+
             window.location.href = "/survey2";
         }
-      
-       
-    },  question5678: function score() {
-        scoreArr2=[];
+
+
+    }, question5678: function score() {
+        scoreArr2 = [];
 
         //code loops 4* for each question in form
         for (i = 5; i < 9; i++) {
@@ -136,102 +136,103 @@ let validateAndStore = {
                     // if qanswer is checked push that value to array
                     scoreArr2.push(question[i]);
                     number = i - 3;
-                 }
+                }
             }
             // validation of all questions answered, highlight next question to be answered
         } if (scoreArr2.length < 4) {
             alert("You must answer all questions to proceed"); scoreArr2 = []; $(".word" + [number]).focus();
-        }if (scoreArr2.length===4){
+        } if (scoreArr2.length === 4) {
             scoreArr2.forEach(element => {
-    
-                $.ajax ({
+
+                $.ajax({
                     type: "POST",
                     url: "/survey111",
-                    data:{ score: element},
+                    data: { score: element },
                     dataType: "json"
-            
+
                 });
             });
-            
-            
-            
-            window.location.href = "/survey3";}
-       
-        
-    }
-    
 
-    }
-    $("#submit").click(function() {
-    userPic= $("#userImg").attr("src")
-    //   console.log(friend)
-        
-      
 
-        $.ajax ({
-            type: "POST",
-            url: "/survey333",
-            data: {pic: userPic},
-            dataType: "json"
-    
-        });
 
-        // analyze user profile against other profiles and post match
-         $.get("/survey33", function(data) {
-   
-  matchedGender=[]
-    
-        var accounts = [];
-        var accountsMatched= []
-    for ( k=0; k<data.friend.length-1; k++){
-        
-        // console.log(data.friend[i]);
-        if (data.friend[k].scores[0]!=data.friend[data.friend.length-1].scores[0]){
-            // console.log(data.friend[data.friend.length-1].scores[0])
-            // // console.log("friend not my gender")
-            matchedGender.push(data.friend[k])
+            window.location.href = "/survey3";
         }
+
+
     }
-    console.log(matchedGender)
-    console.log(matchedGender.length)
-    console.log(matchedGender[0].scores)
-        for (var i=0; i<  matchedGender.length; i++ ){
+
+
+}
+$("#submit").click(function () {
+    userPic = $("#userImg").attr("src")
+    //   console.log(friend)
+
+
+    $("#opClass").fadeTo("fast", 1)
+    $.ajax({
+        type: "POST",
+        url: "/survey333",
+        data: { pic: userPic },
+        dataType: "json"
+
+    });
+
+    // analyze user profile against other profiles and post match
+    $.get("/survey33", function (data) {
+
+        matchedGender = []
+
+        var accounts = [];
+        var accountsMatched = []
+        for (k = 0; k < data.friend.length - 1; k++) {
+
+            // console.log(data.friend[i]);
+            if (data.friend[k].scores[0] != data.friend[data.friend.length - 1].scores[0]) {
+                // console.log(data.friend[data.friend.length-1].scores[0])
+                // // console.log("friend not my gender")
+                matchedGender.push(data.friend[k])
+            }
+        }
+        console.log(matchedGender)
+        console.log(matchedGender.length)
+        console.log(matchedGender[0].scores)
+        for (var i = 0; i < matchedGender.length; i++) {
             accounts[i] = [];
-         for (j=2; j<matchedGender[i].scores.length; j++){
-        //      console.log(data.friend[i].scores[j]+ "all friends not my gender scores 3-8")
-            // array for holding differences of each members score
-            
-        //    console.log (data.friend[i].scores[j])
-           number= (Math.abs(data.friend[data.friend.length-1].scores[j]))-(Math.abs(matchedGender[i].scores[j]));
-        //    console.log(number)
-          accounts[i].push(Math.abs( number))
-        
-        }; 
-       accountsMatched.push( accounts[i].reduce((a, b) => a + b, 0));
-        
-console.log(matchedGender)
+            for (j = 2; j < matchedGender[i].scores.length; j++) {
+                //      console.log(data.friend[i].scores[j]+ "all friends not my gender scores 3-8")
+                // array for holding differences of each members score
 
-    } 
-    console.log(accountsMatched);
-    // console.log(indexMatch)
+                //    console.log (data.friend[i].scores[j])
+                number = (Math.abs(data.friend[data.friend.length - 1].scores[j])) - (Math.abs(matchedGender[i].scores[j]));
+                //    console.log(number)
+                accounts[i].push(Math.abs(number))
+
+            };
+            accountsMatched.push(accounts[i].reduce((a, b) => a + b, 0));
+
+            console.log(matchedGender)
+
+        }
+        console.log(accountsMatched);
+        // console.log(indexMatch)
 
 
-        
-      indexMatch= Math.min(...accountsMatched);
-    userMatchIndex=accountsMatched.indexOf(indexMatch);
-    $("#matchImg").attr("src",matchedGender[userMatchIndex].photo);
-    $("#displayMatchText").text(matchedGender[userMatchIndex].name+ ", cannot wait to meet you!")
-    $("#userName").text(data.friend[data.friend.length-1].name)
-    $("#matchName").text(matchedGender[userMatchIndex].name)
-    
-    // console.log(accountsMatched)
+
+        indexMatch = Math.min(...accountsMatched);
+        userMatchIndex = accountsMatched.indexOf(indexMatch);
+        $("#matchImg").attr("src", matchedGender[userMatchIndex].photo);
+        $("#displayMatchText").text(matchedGender[userMatchIndex].name + ", cannot wait to meet you!")
+        $("#userName").text(data.friend[data.friend.length - 1].name)
+        $("#matchName").text(matchedGender[userMatchIndex].name)
+
+        // console.log(accountsMatched)
         console.log(indexMatch)
-    console.log(userMatchIndex)
+        console.log(userMatchIndex)
     });
 
 
 })
-    
+
 
 $("#next").click(function () {
     if ($(this).data("value") === "firstPage") {
@@ -245,46 +246,50 @@ $("#next").click(function () {
         validateAndStore.question5678();
 
     }
-}); 
+});
 // $("#submit").click( function (){
 //    validateAndStore.upload();
-            
+
 // });
-$(document).ready( function() {
-    $(document).on('change', '.btn-file :file', function() {
-    var input = $(this),
-        label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
-    input.trigger('fileselect', [label]);
+$(document).ready(function () {
+    $(document).on('change', '.btn-file :file', function () {
+        var input = $(this),
+            label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+        input.trigger('fileselect', [label]);
+        
+        
     });
 
-    $('.btn-file :file').on('fileselect', function(event, label) {
-        
+    $('.btn-file :file').on('fileselect', function (event, label) {
+
+
         var input = $(this).parents('.input-group').find(':text'),
             log = label;
-        
-        if( input.length ) {
+
+        if (input.length) {
             input.val(log);
         } else {
-            if( log ) alert(log);
+            if (log) alert(log);
         }
-    
+
     });
     function readURL(input) {
         if (input.files && input.files[0]) {
             var reader = new FileReader();
-            
+
             reader.onload = function (e) {
                 $('#userImg').attr('src', e.target.result);
             }
-            
+
             reader.readAsDataURL(input.files[0]);
         }
 
     }
 
-    $("#imgInp").change(function(){
+    $("#imgInp").change(function () {
+        
         readURL(this);
-       
-    }); 	
+
+    });
     // console.log(readURL(input))
 });
--- server.js
+++ server.js
@@ -12,94 +12,93 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'))
-  
-app.get("/", function(req, res) {
-    res.sendFile(__dirname+ "/public/home.html");
-  });
-  
-  app.get("/survey", function(req, res) {
-    res.sendFile(__dirname+ "/public/survey.html");
-  });
-  
-  app.get("/survey1", function(req, res) {
-    res.sendFile(__dirname+ "/public/survey1.html");
-  });
-  app.get("/survey2", function(req, res) {
-    res.sendFile(__dirname+ "/public/survey2.html");
-  });
-  app.get("/survey3", function(req, res) {
-    res.sendFile(__dirname+ "/public/survey3.html");
-  });
-  //save for later when displaying friend
-  app.get("/survey33", function(req, res) {
-    return res.json(friend);
-  });
 
-  app.post("/survey000", function(req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
-    let pics = req.body;
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/public/home.html");
+});
 
-    friend.friend.push({
-      "name": pics.userNameInput,
-      "photo": "",
-      "scores": [pics.genderValInput, pics.ageGroupInput,
-          
-      ]
+app.get("/survey", function (req, res) {
+  res.sendFile(__dirname + "/public/survey.html");
+});
+
+app.get("/survey1", function (req, res) {
+  res.sendFile(__dirname + "/public/survey1.html");
+});
+app.get("/survey2", function (req, res) {
+  res.sendFile(__dirname + "/public/survey2.html");
+});
+app.get("/survey3", function (req, res) {
+  res.sendFile(__dirname + "/public/survey3.html");
+});
+//save for later when displaying friend
+app.get("/survey33", function (req, res) {
+  return res.json(friend);
+});
+
+app.post("/survey000", function (req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  let pics = req.body;
+
+  friend.friend.push({
+    "name": pics.userNameInput,
+    "photo": "",
+    "scores": [pics.genderValInput, pics.ageGroupInput,
+
+    ]
   })
-    
+
   console.log(friend.friend)
-    
-  });
-  // post question scores 1-8
-  app.post("/survey111", function(req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
-    let score = req.body.score;
-    
+
+});
+// post question scores 1-8
+app.post("/survey111", function (req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  let score = req.body.score;
 
 
-    friend.friend[friend.friend.length-1].scores.push(
-     
-     score
+
+  friend.friend[friend.friend.length - 1].scores.push(
+
+    score
 
 
   )
-    
-  console.log(friend.friend[friend.friend.length-1])
-    
-  });
-  
 
-  app.post("/survey333", function(req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
-    var pic = req.body.pic;
-//  console.log(friend.friend)
-  
+  console.log(friend.friend[friend.friend.length - 1])
+
+});
+
+
+app.post("/survey333", function (req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  var pic = req.body.pic;
+  //  console.log(friend.friend)
+
   console.log(pic)
-    friend.friend[friend.friend.length-1].photo=pic
-      
-      console.log(friend.friend[friend.friend.length-1])
-     
-      
+  friend.friend[friend.friend.length - 1].photo = pic
 
-    
- 
-    
-  });
+  console.log(friend.friend[friend.friend.length - 1])
 
- 
-  
-  
+
+
+
+
+
+});
+
+
+
+
 
 // console.log(friend.friend[0])
 
-  
-  
-  
-  app.listen(PORT, function(){
-      console.log("listening on "+PORT);
-  });
 
-  
+
+
+app.listen(PORT, function () {
+  console.log("listening on " + PORT);
+});
+
